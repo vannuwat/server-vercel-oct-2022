163a77cadab665f9932275da49bfa2bdabda03a4
vannuwat <114803233+vannuwat@users.noreply.github.com>
2022-10-22 21:52:43 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -133,6 +133,29 @@ app.get('/api/database/bus_info/summaryTable/sorted_location/:bus_id', function(
 });
 
 let in_data = "";
+// app.post('/api/database/magellan/sensor', (req, res) => {
+//   let ThingName = req.body.ThingName;
+//   let IMEI= req.body.IMEI;
+//   let IMSI= req.body.IMSI;
+//   let ICCID= req.body.ICCID;
+//   let Sensor= req.body.Sensor;
+//   let sensor_data = Object.values(Sensor);
+//   res.status(200).send(req.body);
+//   in_data = req.body;
+//   res.send(in_data);
+//   var sql=`INSERT INTO test_iot_info (thing_name, IMEI, IMSI, ICCID, total_passenger, in_passenger, out_passenger, location, schedule_date) VALUES ('${ThingName}', '${IMEI}', '${IMSI}', '${ICCID}', ${sensor_data[0]}, ${sensor_data[1]}, ${sensor_data[2]}, 'Unknown', NOW());`;
+//   pool.query(sql, function (err, data) {
+//     if (err){
+//       res.send(err);  
+       
+//     }
+//     else{
+//       res.status(200).send(req.body);  
+//     }
+    
+//   });
+// });
+
 app.post('/api/database/magellan/sensor', (req, res) => {
   let ThingName = req.body.ThingName;
   let IMEI= req.body.IMEI;
@@ -140,10 +163,9 @@ app.post('/api/database/magellan/sensor', (req, res) => {
   let ICCID= req.body.ICCID;
   let Sensor= req.body.Sensor;
   let sensor_data = Object.values(Sensor);
-  res.status(200).send(req.body);
   in_data = req.body;
-  res.send(in_data);
-  var sql=`INSERT INTO test_iot_info (thing_name, IMEI, IMSI, ICCID, total_passenger, in_passenger, out_passenger, location, schedule_date) VALUES ('${ThingName}', '${IMEI}', '${IMSI}', '${ICCID}', ${sensor_data[0]}, ${sensor_data[1]}, ${sensor_data[2]}, 'Unknown', NOW());`;
+
+  var sql=`INSERT INTO test_iot_info (thing_name, IMEI, IMSI, ICCID, total_passenger, in_passenger, out_passenger, location, schedule_date) VALUES ('dummy ThingName', 'dummy IMEI', 'dummy IMSI', 'dummy ICCID', 0, 0, 0, 'Unknown', NOW());`;
   pool.query(sql, function (err, data) {
     if (err){
       res.send(err);  
