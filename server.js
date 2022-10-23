5c0332d09122f23891be5e2547e543cf8b451988
vannuwat <114803233+vannuwat@users.noreply.github.com>
2022-10-23 11:36:40 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,23 +1,130 @@
-//this code is work in magellan AIS
-
 var express = require('express');
 const app = express();
+
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+ 
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    ssl: true,
+    rejectUnauthorized: false,
+})
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
 app.use(express.urlencoded({extended: false}))
 
+
+let initial_sql = 'SELECT * FROM each_bus_info';
+var each_bus_info = null;
+
+pool.query(initial_sql, function (err, data) {
+  if (err){
+      console.log(err)
+  }
+  each_bus_info = data.rows
+  });
+
+
+app.get('/api/database/bus_info/summaryTable', function(req, res) {
+  
+    console.log('Connected!');
+    console.log(req)
+    var sql='SELECT * FROM bus_info';
+    pool.query(sql, function (err, data) {
+    if (err){
+        console.log(err)
+    }
+    res.send(data.rows)
+    });
+  
+});
+
+app.get('/api/database/bus_info/summaryTable/:bus_id', function(req, res) {
+  
+  const found = each_bus_info.some(e => e.bus_id === parseInt(req.params.bus_id));
+
+  if (found) {
+    const id = parseInt(req.params.bus_id);
+    var sql=`SELECT * FROM each_bus_info WHERE bus_id = ${id} ORDER by location,schedule_date ASC`;
+    pool.query(sql, function (err, data) {
+    if (err){
+        console.log(err)
+    }
+    res.send(data.rows)
+    });
+
+  } else {
+
+    res.sendStatus(400);
+
+  }
+
+});
+
+app.get('/api/database/bus_info/summaryTable/sorted_schedule/:bus_id', function(req, res) {
+  
+  const found = each_bus_info.some(e => e.bus_id === parseInt(req.params.bus_id));
+
+  if (found) {
+    const id = parseInt(req.params.bus_id);
+    var sql=`SELECT * FROM each_bus_info WHERE bus_id = ${id} ORDER by schedule_date ASC`;
+    pool.query(sql, function (err, data) {
+    if (err){
+        console.log(err)
+    }
+    res.send(data.rows)
+    });
+
+  } else {
+
+    res.sendStatus(400);
+
+  }
+
+});
+
+app.get('/api/database/bus_info/summaryTable/sorted_location/:bus_id', function(req, res) {
+  
+  const found = each_bus_info.some(e => e.bus_id === parseInt(req.params.bus_id));
+
+  if (found) {
+    const id = parseInt(req.params.bus_id);
+    var sql=`SELECT DISTINCT "location" FROM each_bus_info where bus_id = ${id} order by "location" ;`;
+    pool.query(sql, function (err, data) {
+    if (err){
+        console.log(err)
+    }
+    res.send(data.rows)
+    });
+
+  } else {
+
+    res.sendStatus(400);
+
+  }
+
+});
+
 let in_data = "";
 
 app.post('/api/database/magellan/sensor', (req, res) => {
-  in_data = req.body;
+  ThingName = req.body.ThingInfo.ThingName
+  in_data = req.body.Sensors.temperature
   res.status(200).send({in_data});
 });
 
 app.get('/api/database/magellan/sensor', (req, res) => {
+
   res.status(200).send({in_data});
 });
-
 
 
 
